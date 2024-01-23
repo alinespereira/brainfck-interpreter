004f5b801754440e4ef679bfb2a83a223a808bd9
Aline <74569140+alinespereira@users.noreply.github.com>
2024-01-23 01:16:52 +0000

    Implement Instructions (except loops)

--- src/Brainfck/Interpreter.hs
+++ src/Brainfck/Interpreter.hs
@@ -21,112 +21,61 @@ initializeProgram p = ProgramState
 
 runProgram :: ProgramStateT ()
 runProgram = do
+  runInstruction
+  st <- get
+  unless (isHalt st) $ do
+    runIncrementIp
+    runProgram
+
+
+runInstruction :: ProgramStateT ()
+runInstruction = do
   programState <- get
   case getInstruction programState of
-    Nothing -> return ()
-    Just instruction ->
-      let programState' = 
-        case instruction of
-          Increment -> incrementData programState
-          Decrement -> decrementData programState
-          Input -> undefined
-          Output -> undefined
-          JumpForward -> jumpForward programState
-          JumpBackward -> jumpBackward programState
-          LoopStart -> undefined
-          LoopEnd -> undefined
-      put programState'
-      runProgram
+    Nothing -> runHalt
+    Just instruction -> case instruction of
+      Increment -> runIncrement
+      Decrement -> runDecrement
+      Input -> runInput
+      Output -> runOutput
+      JumpForward -> runJumpForward
+      JumpBackward -> runJumpBackward
+      LoopStart -> runLoopStart
+      LoopEnd -> runLoopEnd
 
--- runProgram :: ProgramStateT ()
--- runProgram = do
---   programState <- get
---   case getInstruction programState of
---     Nothing -> return ()
---     Just i -> do
---       runInstruction i
---       runProgram
+runJumpForward :: ProgramStateT ()
+runJumpForward = modify jumpForward
 
--- runInstruction :: Instruction -> ProgramStateT (Maybe Instruction)
+runJumpBackward :: ProgramStateT ()
+runJumpBackward = modify jumpBackward
 
--- runInstruction JumpForward = do
---   st <- fmap (nextInstruction . jumpForward) get
---   put st
---   return (getInstruction st)
+runIncrement :: ProgramStateT ()
+runIncrement = modify incrementData
 
--- runInstruction JumpBackward =  do
---   st <- fmap (nextInstruction . jumpBackward) get
---   put st
---   return (getInstruction st)
+runDecrement :: ProgramStateT ()
+runDecrement = modify decrementData
 
--- runInstruction Increment = do
---   st <- fmap (nextInstruction . incrementData) get
---   put st
---   return (getInstruction st)
+runLoopStart :: ProgramStateT ()
+runLoopStart = undefined
 
--- runInstruction Decrement = do
---   st <- fmap (nextInstruction . decrementData) get
---   put st
---   return (getInstruction st)
+runLoopEnd :: ProgramStateT ()
+runLoopEnd = undefined
 
--- runInstruction LoopStart = undefined
--- runInstruction LoopEnd = undefined
--- runInstruction Input = do
---   liftIO $ putStr "Enter a character: "
---   ch:_ <- liftIO $ getLine
---   liftIO $ putStrLn $ "Provided value is " ++ (show ch)
---   st <- fmap (nextInstruction . setData (ord ch)) get
---   put st
---   return (getInstruction st)
+runInput :: ProgramStateT ()
+runInput = do
+  liftIO $ putStr "Enter a character: "
+  ch:_ <- liftIO getLine
+  liftIO $ putStrLn $ "Provided value is " ++ show ch
+  st <- fmap (setData (ord ch)) get
+  put st
 
--- runInstruction Output =  do
---   st <- get
---   liftIO $ putChar (chr (getData st))
---   let st' = nextInstruction st
---   put st'
---   return (getInstruction st')
+runOutput :: ProgramStateT ()
+runOutput = do
+  st <- get
+  liftIO $ putChar (chr (getData st))
 
-jumpForward :: ProgramState -> ProgramState
-jumpForward = updateDataPointerWith (+1)
+runIncrementIp :: ProgramStateT ()
+runIncrementIp = modify incrementIp
 
-jumpBackward :: ProgramState -> ProgramState
-jumpBackward = updateDataPointerWith (`subtract` 1)
-
-incrementData :: ProgramState -> ProgramState
-incrementData = updateMemoryWith (+1)
-
-decrementData :: ProgramState -> ProgramState
-decrementData = updateMemoryWith (`subtract` 1)
-
-nextInstruction :: ProgramState -> ProgramState
-nextInstruction = updateInstructionPointerWith (+1)
-
-previousInstruction :: ProgramState -> ProgramState
-previousInstruction = updateInstructionPointerWith (`subtract` 1)
-
-getData :: ProgramState -> Int
-getData (ProgramState _ _ d m) = fromMaybe 0 (S.lookup d m)
-
-setData :: Int -> ProgramState -> ProgramState
-setData x st@(ProgramState _ _ d m) = st { memory = m' }
-  where
-    m' = S.update d x m
-
-getInstruction :: ProgramState -> Maybe Instruction
-getInstruction (ProgramState (Program is) ip _ _) = 
-  S.lookup ip is
-
-updateDataPointerWith :: (Int -> Int) -> ProgramState -> ProgramState
-updateDataPointerWith f st@(ProgramState _ _ d m) =
-  let d' = f d
-      dd = max 0 (d' - length m)
-      m' = m >< S.replicate dd 0
-    in st { dataPointer = d', memory = m' }
-
-updateMemoryWith :: (Int -> Int) -> ProgramState -> ProgramState
-updateMemoryWith f st@(ProgramState _ _ d m) =
-  st { memory = S.adjust f d m }
-
-updateInstructionPointerWith :: (Int -> Int) -> ProgramState -> ProgramState
-updateInstructionPointerWith f st@(ProgramState _ i _ _) =
-  st { instructionPointer = f i }
+runHalt :: ProgramStateT ()
+runHalt = modify halt
